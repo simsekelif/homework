943bda0af941614c02ca54fd4c9c6dd67599823d
simsekelif <kahyaelif@gmail.com>
2024-10-23 23:45:02 +0200

    refactor

--- myDocument.js
+++ myDocument.js
@@ -2,15 +2,7 @@
 
 for(let i = 42; i<= 173; i++) {
     
-
-    let leftFrom13 = i % 13;
-    let leftFrom6 = i % 6; 
-    
-
-    if(leftFrom13 === 0 && leftFrom6 !== 0) {
-
+    if(i % 13 === 0 && i % 6 !== 0) {
         console.log(i);
     } 
-
-
 }
