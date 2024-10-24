d1cd6fbb398295e1d5bdce8aec083efaae2c3d6d
simsekelif <kahyaelif@gmail.com>
2024-10-24 17:22:06 +0200

    initial commit

---
+++ exercise4.js
@@ -0,0 +1,13 @@
+// 4) Initiate variable x as any number between 10 and 16. Using loops and conditions calculate the double factorial (see examples below or the attached link) of that number and print it.
+
+// Example double factorials:
+// 7!! = 7 * 5 * 3 * 1 = 105
+// 8!! = 8 * 6 * 4 * 2 = 384
+// 4!! = 4 * 2 = 8
+// 3!! = 3 * 1 = 3
+
+let x = 11
+for ( let i = x - 2; i >= 1; i -= 2) {
+    console.log(i);
+}
+
